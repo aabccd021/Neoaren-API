fc20d19971248cea8b57cde75ba867d7849571db
aabccd021 <aabccd021@gmail.com>
2018-10-07 18:41:26 +0700

    cathc all

--- src/main/java/hello/Testah.java
+++ src/main/java/hello/Testah.java
@@ -42,64 +42,68 @@ public class Testah {
 
         JSONObject ret = new JSONObject();
 
-        ByteArrayInputStream in = new ByteArrayInputStream(problem.getBytes());
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-        final Object[] args = new Object[1];
-        args[0] = new String[0];
-
-
-        //==================
-        System.setIn(in);
-        System.setOut(new PrintStream(outContent));
-        long startTime = 0;
-        long endTime = 0;
-
-        ExecutorService executor = Executors.newCachedThreadPool();
-        Callable<Object> task = new Callable<Object>() {
-            public Object call() {
-                try {
-                    return helloClass.getMethod("main", String[].class).invoke(null, args);
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (InvocationTargetException e) {
-                    e.printStackTrace();
-                } catch (NoSuchMethodException e) {
-                    e.printStackTrace();
-                }
-                return null;
-            }
-        };
-        Future<Object> future = executor.submit(task);
         try {
-            startTime = System.nanoTime();
-            Object result = future.get(5, TimeUnit.SECONDS);
-        } catch (TimeoutException ex) {
-            timeout = true;
-        } catch (InterruptedException e) {
-        } catch (ExecutionException e) {
-        } finally {
-            endTime = System.nanoTime();
-            duration = (endTime - startTime);
-            future.cancel(true); // may or may not desire this
+            ByteArrayInputStream in = new ByteArrayInputStream(problem.getBytes());
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+            final Object[] args = new Object[1];
+            args[0] = new String[0];
+
+
+            //==================
+            System.setIn(in);
+            System.setOut(new PrintStream(outContent));
+            long startTime = 0;
+            long endTime = 0;
+
+            ExecutorService executor = Executors.newCachedThreadPool();
+            Callable<Object> task = new Callable<Object>() {
+                public Object call() {
+                    try {
+                        return helloClass.getMethod("main", String[].class).invoke(null, args);
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    } catch (NoSuchMethodException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }
+            };
+            Future<Object> future = executor.submit(task);
+            try {
+                startTime = System.nanoTime();
+                Object result = future.get(5, TimeUnit.SECONDS);
+            } catch (TimeoutException ex) {
+                timeout = true;
+            } catch (InterruptedException e) {
+            } catch (ExecutionException e) {
+            } finally {
+                endTime = System.nanoTime();
+                duration = (endTime - startTime);
+                future.cancel(true); // may or may not desire this
+            }
+
+            System.setIn(stdin);
+            System.setOut((PrintStream) stdout);
+            //==================
+
+            answer = outContent.toString().trim().replaceAll(String.valueOf((char) 0x0D), "");
+
+            if ((expected.replaceAll(String.valueOf((char) 0x0D), "")).equals(answer)) {
+                pass = true;
+            }
+
+            ret.put("problem", problem);
+            ret.put("expected", expected);
+            ret.put("answer", answer);
+            ret.put("timeout", timeout);
+            ret.put("pass", pass);
+            ret.put("duration", duration);
+        } catch (Exception e){
+            e.printStackTrace();
         }
-
-        System.setIn(stdin);
-        System.setOut((PrintStream) stdout);
-        //==================
-
-        answer = outContent.toString().trim().replaceAll(String.valueOf((char) 0x0D), "");
-
-        if ((expected.replaceAll(String.valueOf((char) 0x0D), "")).equals(answer)) {
-            pass = true;
-        }
-
-        ret.put("problem", problem);
-        ret.put("expected", expected);
-        ret.put("answer", answer);
-        ret.put("timeout", timeout);
-        ret.put("pass", pass);
-        ret.put("duration", duration);
         return ret;
     }
 }
